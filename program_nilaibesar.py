572d5173c28f29cd28abca5ed5cfc5df607e831b
alyandra-adhytia <alyandraadhytiasopyan01@gmail.com>
2024-11-15 19:09:15 +0700

    program_nilaibesar.py

---
+++ program_nilaibesar.py
@@ -0,0 +1,15 @@
+# dibuat oleh : alyandra adhytia sopyan
+# tanggal dibuat : 15 november 2024
+# program nilai terbesar
+
+print('='*40)
+print('mencari nilai terbesar')
+print('='*40)
+
+n1 = int(input('masukan nilai kesatu : '))
+n2 = int(input('masukan nilai kedua : '))
+n3 = int(input('masukan nilai ketiga : '))
+
+besar = max(n1,n2,n3)
+
+print(f'nilai terbesar adalah {besar}')
