491c2113ef95eb6dd1df291f8c0b07cf23fb447f
alyandra-adhytia <alyandraadhytiasopyan01@gmail.com>
2024-11-15 19:35:25 +0700

    program_konversi waktu.py

---
+++ program_adit100.py/menghitungbilanganganjil/program_konversiwaktu.py
@@ -0,0 +1,9 @@
+# dibuat oleh : alyandra adhytia sopyan
+# dibuat oleh : 15 november 2024
+# Program konversi waktu dari menit ke jam dan menit
+
+total_menit = int(input("Masukkan jumlah menit: "))
+jam = total_menit // 60
+menit = total_menit % 60
+
+print(f"{total_menit} menit = {jam} jam dan {menit} menit.")
