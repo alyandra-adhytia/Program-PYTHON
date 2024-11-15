fc0e14b1759e85198ea9e17cd1c6083e34b8e1c8
alyandra-adhytia <alyandraadhytiasopyan01@gmail.com>
2024-11-15 18:55:58 +0700

    program skala

---
+++ program_skala.py
@@ -0,0 +1,11 @@
+# dibuat oleh : alyandra adhytia sopyan
+# tanggal di buat : november 2024
+# program skala peta
+
+print('='*30)
+print('menghitung skala')
+print('='*30)
+
+jarak = float(input("masukan jarak pada peta(dalam cm) :"))
+skala = float(input("masukan skala peta: 1/"))
+print (jarak*skala/10000,"km")
